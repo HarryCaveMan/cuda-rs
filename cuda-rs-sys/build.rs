4930638d027553c50d275eb2a6fe34bcb2bccebf
HarryCaveMan <harris.joseph.088@gmail.com>
2025-12-22 19:39:32 -0500

    updating lib build for v13

--- cuda-rs-sys/build.rs
+++ cuda-rs-sys/build.rs
@@ -32,7 +32,7 @@ fn main() {
 
     println!("cargo:rustc-link-arg=-Wl,-rpath,{}", cuda_lib_dir.to_string_lossy());
     println!("cargo:rustc-link-arg=-L{}", cuda_lib_dir.to_string_lossy());
-    println!("cargo:rustc-link-lib=cuda");
+    println!("cargo:rustc-link-lib=cudart");
 
     let bindings = bindgen::Builder::default()
         .clang_arg(format!("-I{}", cuda_include.to_string_lossy()))
